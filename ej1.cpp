5ac7c1b9e096118b9fa970cf39a991cb2afac9a2
gaspush <pizzogaspar@gmail.com>
2023-06-14 12:07:51 -0300

    emprolijamiento, sigue WA

--- ej1.cpp
+++ ej1.cpp
@@ -6,61 +6,37 @@ using namespace std;
 
 int casos, n, m, k, salida, llegada;
 vector<tuple<int,int,int>> kaes;
-vector<int> distancias, predecesores, procesado, predecesoresT, distanciasT, procesadoT;
-vector<vector<tuple<int,int>>> adyacencias;
+vector<int> distanciasS, distanciasT, procesados;
+vector<vector<tuple<int,int>>> adyacenciasS;
 vector<vector<tuple<int,int>>> adyacenciasT;
 
 bool const comparar(const tuple<int,int> a, const tuple<int,int> b){
     return get<1>(a) > get<1>(b);
 }
 
-priority_queue<tuple<int,int>, vector<tuple<int,int>>, decltype(&comparar)> Qs(comparar);
-priority_queue<tuple<int,int>, vector<tuple<int,int>>, decltype(&comparar)> Qt(comparar);
+void dijkstra(vector<int> &dist, vector<vector<tuple<int,int>>> &ady){
 
-void dijkstra(){
+    priority_queue<tuple<int,int>, vector<tuple<int,int>>, decltype(&comparar)> Q(comparar);
 
     for (int i = 1; i <= n; i++){
-        tuple<int,int> temp = {i,distancias[i]};
-        Qs.push(temp);
+        tuple<int,int> temp = {i,dist[i]};
+        Q.push(temp);
     }
-    while (!Qs.empty())
+    while (!Q.empty())
     {
-        int a=get<0>(Qs.top());
-        Qs.pop();
-        if (procesado[a]) continue;
-        procesado[a]=true;
-        for (auto u : adyacencias[a]) {
+        int a=get<0>(Q.top());
+        Q.pop();
+        if (procesados[a]) continue;
+        procesados[a]=true;
+        for (auto u : ady[a]) {
             int b = get<0>(u), w = get<1>(u);
-            predecesores[b]=a;
-            if (distancias[a]+w < distancias[b]) {
-                distancias[b] = distancias[a]+w;
-                tuple<int,int> temp = {b,distancias[b]};
-                Qs.push(temp);
+            if (dist[a]+w < dist[b]) {
+                dist[b] = dist[a]+w;
+                tuple<int,int> temp = {b,dist[b]};
+                Q.push(temp);
             }
         }
     }
-}
-void dijkstra2(){
-
-    for (int i = 1; i <= n; i++){
-        tuple<int,int> temp = {i,distanciasT[i]};
-        Qt.emplace(temp);
-    }
-    while (!Qt.empty())
-    {
-        int a=get<0>(Qt.top());
-        Qt.pop();
-        if (procesadoT[a]) continue;
-        procesadoT[a]=true;
-        for (auto u : adyacenciasT[a]) {
-            int b = get<0>(u), w = get<1>(u);
-            if (distanciasT[a]+w < distanciasT[b]) {
-                distanciasT[b] = distanciasT[a]+w;
-                tuple<int,int> temp = {b,distanciasT[b]};
-                Qt.push(temp);
-            }            
-        }
-    }    
 }
 
 int main(){
@@ -70,16 +46,13 @@ int main(){
 
         cin >> n >> m >> k >> salida >> llegada;
 
-        predecesores = vector<int> (n+1,-1);
-        predecesoresT = vector<int> (n+1,-1);
-        procesado = vector<int> (n+1, false);
-        procesadoT = vector<int> (n+1,false);
-        distancias = vector<int> (n+1,1000*n);
+        procesados = vector<int> (n+1, false);
+        distanciasS = vector<int> (n+1,1000*n);
         distanciasT = vector<int> (n+1,1000*n); //para correr Dijks desde t
-        adyacencias = vector<vector<tuple<int,int>>> (n+1);
+        adyacenciasS = vector<vector<tuple<int,int>>> (n+1);
         adyacenciasT = vector<vector<tuple<int,int>>> (n+1);
 
-        distancias[salida] = 0;
+        distanciasS[salida] = 0;
         distanciasT[llegada] = 0;
 
         for (int i = 0; i < m; i++){
@@ -87,7 +60,7 @@ int main(){
             cin >> u >> v >> w;
             tuple<int,int> tup = {v,w};
             tuple<int,int> tup2 = {u,w}; // grafo dado vuelta para correr Dijks desde t
-            adyacencias[u].emplace_back(tup);
+            adyacenciasS[u].emplace_back(tup);
             adyacenciasT[v].emplace_back(tup2); 
         }
 
@@ -98,10 +71,11 @@ int main(){
             kaes.emplace_back(tup);
         }
 
-        dijkstra();
-        dijkstra2();
+        dijkstra(distanciasS,adyacenciasS);
+        procesados.assign(n+1,false);
+        dijkstra(distanciasT,adyacenciasT);
 
-        int distanciaMinima = distancias[llegada];
+        int distanciaMinima = distanciasS[llegada];
 
         for(int i = 0; i < kaes.size(); i++){
             tuple<int,int,int> k = kaes[i];
@@ -109,8 +83,8 @@ int main(){
             int v = get<1>(k);
             int w = get<2>(k);
             
-            int distUV = distancias[u] + distanciasT[v] + w;
-            int distVU = distancias[v] + distanciasT[u] + w;
+            int distUV = distanciasS[u] + distanciasT[v] + w;
+            int distVU = distanciasS[v] + distanciasT[u] + w;
 
             if (distUV < distanciaMinima) distanciaMinima = distUV;
             if (distVU < distanciaMinima) distanciaMinima = distVU;
