2a0015e9c51f0ee28a6a89464e0c944d03866b7c
gaspush <pizzogaspar@gmail.com>
2023-06-14 18:31:00 -0300

    ya no hay vector de kaes, igual estoy comiteando gilada para sentir que estoy avanzando pero no estaria siendo el caso

--- ej1.cpp
+++ ej1.cpp
@@ -4,12 +4,10 @@
 #include <queue>
 using namespace std;
 
-int casos, n, m, k, salida, llegada;                //Parametros de entrada
-vector<tuple<int,int,int>> kaes;                    //Vector para las calles bidireccionales 
-vector<int> distanciasS, distanciasT;               //Vectores para registrar las distancias de S a todos y de todos a T
-vector<bool> procesados;                            //Indica si un nodo fue procesado por Dijkstra (tendremos nodos repetidos en la queue)
-vector<vector<tuple<int,int>>> adyacenciasS;        //Lista de adyacencias
-vector<vector<tuple<int,int>>> adyacenciasT;        //Lista de adyacencias inversa
+int casos, n, m, k, salida, llegada;                        // Parametros de entrada
+vector<int> distanciasS, distanciasT;                       // Vectores para registrar las distancias de S a todos y de todos a T
+vector<bool> procesados;                                    // Indica si un nodo fue procesado por Dijkstra (tendremos nodos repetidos en la queue)
+vector<vector<tuple<int,int>>> adyacenciasS, adyacenciasT;  // Lista de adyacencias normal e inversa (para calcular las distancias de todos a T)
 
 bool const comparar(const tuple<int,int> a, const tuple<int,int> b){
     return get<1>(a) > get<1>(b);
@@ -17,9 +15,9 @@ bool const comparar(const tuple<int,int> a, const tuple<int,int> b){
 
 void dijkstra(vector<int> &dist, vector<vector<tuple<int,int>>> &ady){
 
-    priority_queue<tuple<int,int>, vector<tuple<int,int>>, decltype(&comparar)> Q(comparar);
+    priority_queue<tuple<int,int>, vector<tuple<int,int>>, decltype(&comparar)> Q(comparar); 
 
-    for (int i = 1; i <= n; i++){
+    for (int i = 1; i <= n; i++){           // Creamos un cola de prioridad de tuplas <nodo, distancia a S>
         tuple<int,int> temp = {i,dist[i]};
         Q.push(temp);
     }
@@ -27,8 +25,8 @@ void dijkstra(vector<int> &dist, vector<vector<tuple<int,int>>> &ady){
     {
         int a=get<0>(Q.top());
         Q.pop();
-        if (procesados[a]) continue;
-        procesados[a]=true;
+        if (procesados[a]) continue;        // Como la cola de prioridad no se actualiza, tendremos nodos repetidos...
+        procesados[a]=true;                 // y debemos chequear no estar analizando un nodo ya procesado.
         for (auto u : ady[a]) {
             int b = get<0>(u), w = get<1>(u);
             if (dist[a]+w < dist[b]) {
@@ -52,7 +50,6 @@ int main(){
         distanciasT = vector<int> (n+1,1000*n); // a la distancia del maximo camino posible (n-1 * 1000)
         adyacenciasS = vector<vector<tuple<int,int>>> (n+1);
         adyacenciasT = vector<vector<tuple<int,int>>> (n+1);
-        kaes.clear();
 
         distanciasS[salida] = 0;
         distanciasT[llegada] = 0;
@@ -66,33 +63,24 @@ int main(){
             adyacenciasT[v].emplace_back(tup2); 
         }
 
-        for (int i = 0; i < k; i++){
-            int u, v, w;
-            cin >> u >> v >> w;
-            tuple<int,int,int> tup = {u,v,w};
-            kaes.emplace_back(tup);
-        }
-
-        dijkstra(distanciasS,adyacenciasS);
+        dijkstra(distanciasS,adyacenciasS); // Dijkstra para conocer las distancias de S a todos
         procesados.assign(n+1,false);
-        dijkstra(distanciasT,adyacenciasT);
+        dijkstra(distanciasT,adyacenciasT); // Dijkstra para conocer las distnacias de todos a T
 
         int distanciaMinima = distanciasS[llegada];
 
-        for(int i = 0; i < kaes.size(); i++){
-            tuple<int,int,int> calle = kaes[i];
-            int u = get<0>(calle);
-            int v = get<1>(calle);
-            int w = get<2>(calle);
+        for(int i = 0; i < k; i++){         // Revisamos para cada una de las calles bidireccionales entre U y V con peso W...
+            int u, v, w;
+            cin >> u >> v >> w;
             
             int distUV = distanciasS[u] + distanciasT[v] + w;
             int distVU = distanciasS[v] + distanciasT[u] + w;
 
-            if (distUV < distanciaMinima) distanciaMinima = distUV;
-            if (distVU < distanciaMinima) distanciaMinima = distVU;
+            if (distUV < distanciaMinima) distanciaMinima = distUV; // ...si la distancia mejora recorriendo S -> U -> V -> T
+            if (distVU < distanciaMinima) distanciaMinima = distVU; //          o si es conveniente recorrer S -> V -> U -> T
         }
 
-        if (distanciaMinima == 1000*n) cout << -1 << endl;
+        if (distanciaMinima == 1000*n) cout << -1 << endl; // Si la distancia no cambio desde su inicializacion, el nodo T no es alcanzable desde S
         else cout << distanciaMinima << endl;
 
     }
